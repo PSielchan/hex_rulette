8c9ecd495d6edfaf53d922e5e5a1ab4eb5f5e21b
Horacjusz <pawel.prus05@gmail.com>
2025-04-05 17:22:37 +0200

    curses

--- scripts/main.gd
+++ scripts/main.gd
@@ -8,8 +8,11 @@ extends Node
 enum Action {Shot, Other}
 enum Players {Hero,Enemy}
 
-var buffer = Array()
+const HEALTHY = -1
 
+var buffer = Array()
+var h_curses = [HEALTHY, HEALTHY, HEALTHY, HEALTHY]
+var e_curses = [HEALTHY, HEALTHY, HEALTHY, HEALTHY]
 var next_action = Action.Shot
 var avaible_action = true
 var do_action = false
@@ -18,21 +21,26 @@ var shot = false
 var who_is_shot
 var blasphemy = 0
 
+var current_bullet = 0
 
 func _ready() :
 	buffer.resize(10)
-	var bullet=rand_bulet()
-	for i in range(buffer.size()):
-		if i==bullet:
-			buffer[i]=true
-		else:
-			buffer[i] = false
+	rand_bullets()
 
 var tura = 0
 
 func rand_bulet() :
 	return int(randf()*10)
-
+	
+func rand_bullets():
+	var bullet=rand_bulet()
+	for i in range(buffer.size()):
+		if i==bullet:
+			buffer[i]=true
+		else:
+			# TO CHAGE TO FALSE
+			buffer[i] = true
+	
 func _process(_delta) :
 	if do_action:
 		do_action=false
@@ -55,6 +63,7 @@ func _process(_delta) :
 
 func _input(event) :
 	if event is InputEventMouseButton and event.pressed :
+		print(h_curses)
 		pass
 		#print(event.button_index)
 		#if event.button_index == BUTTON_LEFT :
@@ -75,21 +84,22 @@ func _on_timer_3_timeout() -> void:
 	
 func _on_timer_timeout_1() -> void:
 	timer.stop()
-	queue.update_queue()
 	if who_is_shot == Players.Enemy:
-		update_enemy()
+		if buffer[current_bullet]:
+			update_enemy()
 		print("Atak")
 	if who_is_shot == Players.Hero:
-		update_me()
+		if buffer[current_bullet]:
+			update_me()
 		print("Przyjmuje na klate")
 		blasphemy += 1
-		
 		if blasphemy == 3 :
 			var time_in_seconds = 5
 			await get_tree().create_timer(time_in_seconds).timeout
 			blasphemy = 0
 			
 			#show_choice
+	queue.update_queue()
 	next_action=Action.Other
 	
 	
@@ -101,7 +111,18 @@ func _on_timer_2_timeout() -> void:
 	avaible_action=true
 
 func update_me():
-	pass
+	var current_cure=queue.take()
+	for i in range(4):
+		if h_curses[i] == HEALTHY:
+			h_curses[i] = current_cure
+			return
+	#end game
 
 func update_enemy():
+	var current_cure=queue.take()
+	for i in range(4):
+		if e_curses[i] == HEALTHY:
+			e_curses[i] = current_cure
+			return
+	#win game
 	pass
