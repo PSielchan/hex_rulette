f22a90de9f91c1e3791c86f7eee2ee545b2e4421
Horacjusz <pawel.prus05@gmail.com>
2025-04-05 13:14:43 +0200

    queue done, and some of the background

--- scripts/queue_body.gd
+++ scripts/queue_body.gd
@@ -1,25 +1,33 @@
 extends Sprite2D
 
-#@onready var Main: Node = %Main
-
+var POSITION = $".".position
 
 func rand_curse_id() :
 	return int(randf()*4)
 
 var buffer = [rand_curse_id(), rand_curse_id(), rand_curse_id(), rand_curse_id(), rand_curse_id()]
 
-var textures = [preload("res://assets/queue/curse_0.png"),
-				preload("res://assets/queue/curse_1.png"),
-				preload("res://assets/queue/curse_2.png"),
-				preload("res://assets/queue/curse_3.png"),
+const agnoia_file = "res://assets/queue/agnoia.png"
+const chronos_file = "res://assets/queue/chronos.png"
+const fatum_file = "res://assets/queue/fatum.png"
+const hybris_file = "res://assets/queue/hybris.png"
+
+var textures = [preload(agnoia_file),
+				preload(chronos_file),
+				preload(fatum_file),
+				preload(hybris_file),
 				]
 
-#func _ready() :
-	## Change child textures when ready, or call this function elsewhere
-	#change_children_images()
+#var curses_positions
+
+func _ready() :
+	# Change child textures when ready, or call this function elsewhere
+	
+	change_children_images()
+
 
 func change_children_images() :
-	get_node("../AnimationPlayer").play("slide")
+	$AnimationPlayer.play("slide")
 	var children = get_children()
 	for i in len(buffer) :
 		children[i].texture = textures[buffer[i]]
@@ -33,7 +41,7 @@ func _input(event) :
 		if len(buffer) > 0 :
 			buffer.remove_at(0)
 		buffer.append(rand_curse_id())
-		print(buffer)
+		#print(buffer)
 		change_children_images()
 			
 		#if event.button_index == BUTTON_LEFT :
