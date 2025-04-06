db3c48036a67edbccad366fd25f95feae85c4e0e
PSielchan <147639332+PSielchan@users.noreply.github.com>
2025-04-06 12:20:58 +0200

    duzo

---
+++ scripts/blasphemy_clover.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@onready var main: Node = %Main
+@onready var flaga: Sprite2D = $flaga
+@onready var znaczek: Sprite2D = $znaczek
+
+func _process(_delta: float) -> void:
+	if main.szare[0]:
+		flaga.texture=preload("res://assets/blasfemy/unavaible.png")
+		znaczek.texture=preload("res://assets/blasfemy/clover_u.png")
+	else:
+		flaga.texture=preload("res://assets/blasfemy/avaible.png")
+		znaczek.texture=preload("res://assets/blasfemy/clover_a.png")
+		
+
+
+func _on_button_pressed() -> void:
+	main.blas_clicked=true
+	main.curr_buff=1
